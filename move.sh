fcf242514bdaa07b441b96d384a434302033430e
tusaveeiei <tusaveeiei@gmail.com>
2024-10-05 14:02:04 +0700

    update images

--- move.sh
+++ move.sh
@@ -1,1 +1,21 @@
+# MOVE
 find . -mindepth 2 -type f -exec mv {} ./ \;
+
+# RENAME
+for n in {1..100}; do
+    if [ -f "GIF-${n}-idle_${n}.gif" ]; then
+        mv "GIF-${n}-idle_${n}.gif" "${n}.gif"
+    fi
+done
+
+for n in {1..100}; do
+    if [ -f "GIF-${n}-walk_${n}.gif" ]; then
+        mv "GIF-${n}-walk_${n}.gif" "${n}_walk.gif"
+    fi
+done
+
+for n in {1..100}; do
+    if [ -f "GIF-${n}-walk_r_${n}.gif" ]; then
+        mv "GIF-${n}-walk_r_${n}.gif" "${n}_rush.gif"
+    fi
+done
